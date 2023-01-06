2c385aa17d385b20eddd8eeac7c6b56478dcf663
Tim Peters <zomg.tim@gmail.com>
2023-01-06 12:59:50 +1030

    Add aliases for controlling VMs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,6 +33,42 @@ alias v="$EDITOR"
 alias vnc="vncserver -geometry 1366x768 -SecurityTypes X509Vnc :1"
 alias vrc="$EDITOR ~/.config/fish/config.fish"
 alias vlocal="$EDITOR ~/.config/fish/conf.d/local.fish"
+alias xmlformat="xmllint -format"
+
+if command -sq VBoxManage
+    function shutdown-if-up
+        set -l host $argv[1]
+        set -l uuid $argv[2]
+        echo "== Trying to shutdown $host ($uuid)"
+        echo "VBoxManage list runningvms | grep $uuid"
+        if VBoxManage list runningvms | grep $uuid
+            if ssh $host w | grep " 0 users"
+                ssh $host "sudo shutdown now"
+            else
+                echo "== Cannot shutdown $host, there are logged in users"
+                return 1
+            end
+        else
+            echo "== $host is not running"
+        end
+        return 0
+    end
+
+    function wait-for-vms
+        echo "== Waiting for VMs to shut down"
+        while true
+            set -l count (VBoxManage list runningvms | wc -l)
+            echo "Running VMs: $count"
+            if [ $count = "0" ]
+                break
+            end
+            sleep 1
+        end
+        echo "== All VMs stopped"
+    end
+
+    alias runningvms="VBoxManage list runningvms"
+end
 
 function .rc
     source ~/.config/fish/config.fish
