36992f9ebe5dc7b90dff74e24cc3c3da6eeb04d9
Tim Peters <tim@catminion.net>
2023-06-25 14:55:54 +0930

    mapleader comma

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
--- Set <space> as the leader key
+-- Set <comma> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
