4a968d05f9a05f5f7bfc78b43a05a589fb0336bf
Tim Peters <tim@catminion.net>
2023-06-25 15:30:58 +0930

    Show full path in lualine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -113,6 +113,12 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = { { 'filename', path = 1 } }
+      },
+      inactive_sections = {
+        lualine_c = { { 'filename', path = 1 } }
+      }
     },
   },
 
