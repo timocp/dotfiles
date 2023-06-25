90c3f95c12ffc1d1d16b698537a6fa87afcec342
Tim Peters <tim@catminion.net>
2023-06-25 16:02:16 +0930

    Add @t macro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -526,5 +526,9 @@ cmp.setup {
   },
 }
 
+-- macros
+-- @t swap ' strings for "
+vim.fn.setreg('t', 'r"f\'r"')
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
