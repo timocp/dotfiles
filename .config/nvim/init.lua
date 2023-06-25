56c4d5840bc23af8d6ee65c6fee47ab2b39a3da5
Tim Peters <tim@catminion.net>
2023-06-25 16:15:44 +0930

    Autoinstall treesitter config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -327,8 +327,8 @@ require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
-  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
+  -- Autoinstall languages that are not installed.
+  auto_install = true,
 
   highlight = { enable = true },
   indent = { enable = true },
